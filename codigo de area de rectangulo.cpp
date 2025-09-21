e59e65d930e251e51ca2060e7483b7817315749c
Alfaro <diegopena5294@gmail.com>
2025-09-20 21:16:07 -0600

    Add files via upload

---
+++ codigo de area de rectangulo.cpp
@@ -0,0 +1,34 @@
+/*codigo de area de rectangulo*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int b=4;
+int h=2;
+int area=b*h;
+int perimetro=2*(b+h);
+
+    cout<<"el area del rectangulo es:"<<area;
+    cout<<"el perimetro del rectangulo es:"<<perimetro;
+
+    return 0;
+}
+
+/*codigo de area de triangulo*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int b=4;
+int h=2;
+int l1=4;
+int l2=4;
+int l3=4;
+int area=b*h/2;
+int perimetro=l1+l2+l3;
+
+    cout<<"el area del triangulo es:"<<area<<;
+    cout<<"el perimetro del tirangulo es:"<<perimetro;
+
+    return 0;
+}
